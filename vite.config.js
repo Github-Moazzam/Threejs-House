ee3dbec2a6c72dc158d6be72d09657dd466970ef
Moazzam <khanmoazzam329@gmail.com>
2024-09-13 00:00:49 +0500

    build add

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ export default {
     root: 'src/',
     publicDir: '../static/',
     base: './',
-    build: {
-        outDir: '../dist'  // Ensure output is written to 'dist'
-      }
+
+      build: {
+    outDir: 'dist',  // This is the default setting
+  },
 }
