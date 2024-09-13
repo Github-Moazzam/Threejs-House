5be7bdf6641c1a621454018052115fe6c41b2144
DeBlockX <157020427+DeBlockX@users.noreply.github.com>
2024-09-13 20:39:47 +0500

    new

--- src/script.js
+++ src/script.js
@@ -20,22 +20,54 @@ const sizes = {
 const scene = new THREE.Scene();
 
 //Textures load
-const grassTexture = textureLoader.load(
-  "/textures/whispy-grass-meadow-bl/wispy-grass-meadow_albedo.png"
-);
+const grassAlbedo = textureLoader.load(
+  "/textures/whispy-grass-meadow-bl/wispy-grass-meadow_albedo.png");
+const grassAo = textureLoader.load('/textures/whispy-grass-meadow-bl/wispy-grass-meadow_ao.png');
+const grassHeight = textureLoader.load('/textures/whispy-grass-meadow-bl/wispy-grass-meadow_height.png');
+const grassMetallic = textureLoader.load('/textures/whispy-grass-meadow-bl/wispy-grass-meadow_metallic.png');
+const grassNormal = textureLoader.load('/textures/whispy-grass-meadow-bl/wispy-grass-meadow_normal-ogl.png');
+const grassRoughness = textureLoader.load('/textures/whispy-grass-meadow-bl/wispy-grass-meadow_roughness.png');
+
+
 const boxTexture = textureLoader.load(
   "/textures/bricks-mortar-bl/bricks-mortar-albedo.png"
 );
+const boxAo =  textureLoader.load('/textures/bricks-mortar-bl/bricks-mortar-ao.png')
+const boxHeight =  textureLoader.load('/textures/bricks-mortar-bl/bricks-mortar-height.png')
+const boxMetallic =  textureLoader.load('/textures/bricks-mortar-bl/bricks-mortar-metallic.png')
+const boxNormal =  textureLoader.load('/textures/bricks-mortar-bl/bricks-mortar-normal-ogl.png')
+const boxRoughness = textureLoader.load('/textures/bricks-mortar-bl/bricks-mortar-roughness.png')
+
+
+
 const gateTexture = textureLoader.load("/textures/pngwing.com.png");
+
+
 const roofTexture = textureLoader.load(
   "/textures/grey-asphalt-shingles-bl/grey-asphalt-shingles_albedo.png"
 );
+const roofAo = textureLoader.load(
+  "/textures/grey-asphalt-shingles-bl/grey-asphalt-shingles_ao.png"
+);
+const roofHeight = textureLoader.load(
+  "/textures/grey-asphalt-shingles-bl/grey-asphalt-shingles_height.png"
+);
+const roofMetallic = textureLoader.load(
+  "/textures/grey-asphalt-shingles-bl/grey-asphalt-shingles_metallic.png"
+);
+const roofNormal = textureLoader.load(
+  "/textures/grey-asphalt-shingles-bl/grey-asphalt-shingles_normal-ogl.png"
+);
+const roofroughness = textureLoader.load(
+  "/textures/grey-asphalt-shingles-bl/grey-asphalt-shingles_roughness.png"
+);
 
-grassTexture.repeat.set(2, 2);
-grassTexture.wrapS = THREE.RepeatWrapping;
-grassTexture.wrapT = THREE.RepeatWrapping;
 
-pane.addBinding(grassTexture, "offset", {
+grassAlbedo.repeat.set(3, 3);
+grassAlbedo.wrapS = THREE.RepeatWrapping;
+grassAlbedo.wrapT = THREE.RepeatWrapping;
+
+pane.addBinding(grassAlbedo, "offset", {
   x: {
     min: -1,
     max: 1,
@@ -53,19 +85,42 @@ const geometry = new THREE.BoxGeometry(1, 1, 1);
 const planeGeometry = new THREE.PlaneGeometry(1, 1);
 const gateGeometry = new THREE.PlaneGeometry(1.3, 2.5);
 const roofGeometry = new THREE.ConeGeometry(4, 2, 4, 1, false);
+const sphereGeometry = new THREE.SphereGeometry(1,42,42)
 
 //Material
-const material = new THREE.MeshBasicMaterial();
-material.map = grassTexture;
+const material = new THREE.MeshStandardMaterial();
+material.map = grassAlbedo;
+material.roughnessMap = grassRoughness;
+material.aoMap = grassAo
+material.displacementMap = grassHeight
+material.displacementScale=0.3
+material.metalnessMap = grassMetallic
+material.normalMap = grassNormal
 
-const boxMaterial = new THREE.MeshBasicMaterial();
+const boxMaterial = new THREE.MeshPhysicalMaterial();
 boxMaterial.map = boxTexture;
+boxMaterial.aoMap = boxAo
 
-const gateMaterial = new THREE.MeshBasicMaterial();
+boxMaterial.roughnessMap = boxRoughness;
+boxMaterial.displacementMap = boxHeight
+boxMaterial.displacementScale=0
+boxMaterial.metalnessMap = boxMetallic
+boxMaterial.normalMap = boxNormal
+
+
+const gateMaterial = new THREE.MeshStandardMaterial;
 gateMaterial.map = gateTexture;
 
-const roofMaterial = new THREE.MeshBasicMaterial();
+const roofMaterial = new THREE.MeshStandardMaterial();
 roofMaterial.map = roofTexture;
+roofMaterial.aoMap = roofAo
+roofMaterial.roughnessMap = roofroughness;
+roofMaterial.displacementMap = roofHeight
+roofMaterial.displacementScale=0
+roofMaterial.metalnessMap = roofMetallic
+roofMaterial.normalMap = roofNormal
+
+
 
 const group = new THREE.Group();
 const group2 = new THREE.Group();
@@ -101,10 +156,13 @@ const roof2 = new THREE.Mesh(roofGeometry, roofMaterial);
 roof2.position.set(10, 5.52, 0);
 roof2.rotation.y = Math.PI * 0.25;
 
+const sphere = new THREE.Mesh(sphereGeometry,material)
+sphere.position.set(0,10,0)
+
 
 group.add(box1, plane, gate, roof);
 group2.add(box2,gate2,roof2)
-parentGroup.add(group,group2)
+parentGroup.add(group,group2,sphere)
 
 
 
@@ -112,12 +170,12 @@ parentGroup.add(group,group2)
 const light = new THREE.AmbientLight(0xffffff, 0.4);
 scene.add(light);
 
-const pointLight = new THREE.PointLight(0xffffff, 1.2);
-pointLight.position.set(0, 4, 0);
+const pointLight = new THREE.PointLight('white', 20);
+pointLight.position.set(0, 4.3, 2.51);
 scene.add(pointLight);
 
 const axes = new THREE.AxesHelper(100);
-scene.add(axes)
+parentGroup.add(axes)
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
@@ -157,7 +215,7 @@ const tick = () => {
   previousTime = currentTime;
 
 
-  parentGroup.rotation.y += delta * 1;
+  // parentGroup.rotation.y += delta * 1;
 
   renderer.render(scene, camera);
   window.requestAnimationFrame(tick);
